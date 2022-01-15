df5a548d03bd39b29f3e66d38a4badf0f83728d0
Dor Ben Dov <6714196+doribd@users.noreply.github.com>
2022-01-15 15:45:57 +0000

    adding some conditions and validations

--- getting_input/userinput.go
+++ getting_input/userinput.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
+// accessible not only to main, but all the functions within this package
+const minimumNameSize int = 2
+const minimumMobileNumber int = 10
 
-	var userName string
-	var userMobile string
+var userName string
+var userMobile string
+
+func main() {
 
 	// get input from the user
 	fmt.Println("Please enter you user name:")
@@ -15,5 +19,12 @@ func main() {
 	// only when using the pointer it will work of course
 	fmt.Scan(&userMobile)
 
-	fmt.Printf("Your name is %v and your mobile is %v", userName, userMobile)
+	isValidName := len(userName) >= minimumNameSize
+	isValidMobile := len(userMobile) >= minimumMobileNumber
+
+	if isValidMobile && isValidName {
+		fmt.Printf("Your name is %v and your mobile is %v", userName, userMobile)
+	} else {
+		fmt.Println("Wrong name or mobile number")
+	}
 }
