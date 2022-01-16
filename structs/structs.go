8f9d4d91f74939f42c4111feb4bca495ce1b7ec2
Dor Ben Dov <doribd@gmail.com>
2022-01-16 16:33:42 +0200

    embedded structs

--- structs/structs.go
+++ structs/structs.go
@@ -12,7 +12,25 @@ type User struct {
 	isOptIn   bool
 }
 
+type Foo struct {
+	A int
+	B string
+}
+
+type Bar struct {
+	C string
+	F Foo
+}
+
+type Baz struct {
+	D string
+	// embedded struct within the struct, allows direct access to its functions
+	Foo
+}
+
 func main() {
+
+	// embedding is delegation and not inheritance
 
 	// map can support only 1 data type, structure can hold mixed types
 	var userData = User{
@@ -28,4 +46,13 @@ func main() {
 	fmt.Println(userData.email)
 	fmt.Println(userData.number)
 	fmt.Println(userData.isOptIn)
+
+	f := Foo{A: 10, B: "Hello"}
+	b1 := Bar{C: "Dor", F: f}
+	fmt.Println(b1.F.A)
+
+	b2 := Baz{D: "Dor1", Foo: f}
+	fmt.Println(b2.A)
+	var f2 Foo = b2.Foo
+	fmt.Println(f2)
 }
